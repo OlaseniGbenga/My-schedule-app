8720946c62d851826f92b9dbf2249a009a6c4a4b
Papi <gj.olaseni@gmail.com>
2023-03-08 16:11:03 +0100

    the schedule form and displaying

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,77 +9,94 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function Body() {
-  const [task, setTask] = useState([]);
-  const [todo, setTodo] =useState("");
-  const [time, setTime] = useState("")
-  const [isFormVisible, setIsFormVisible] = useState(false);
+//store data with use state
+const [task, setTask] = useState([]
+);
 
-  const taskHandler = () => {
-    setTask([
-      ...task,
-      { item: "Read and write", time: "3:30pm" },
-      { item: "Read and write", time: "3:30pm" },
-      { item: "Read and write", time: "3:30pm" },
-    ]);
+//control form visibility
+const [isFormVisible, setIsFormVisible] = useState(false);
+
+
+
+//function to update setTask
+function taskHandler(event) {
+  event.preventDefault();
+  const newInput = {
+    todo: event.target.todo.value,
+    time: event.target.time.value,
   };
+  setTask([...task,newInput]
+    
+    
+  );
+  setIsFormVisible(false)
+  event.target.reset();
+  console.log(task)
+}
+
 
   return (
     <div className="min-h-screen font-Roboto flex flex-col gap-y-4  items-center">
       <h3 className="text-xl">To-dos</h3>
-      How a todo should look like
+      {/* How a todo should look like */}
+
       
-        {task.map((activity, index) => (
- <div className=" bg-c-lightBlue p-5 rounded flex gap-4 items-center">
- <FontAwesomeIcon
-       name="menu"
-       icon={faCircleDot}
-       className="text-c-white"
-       size="1x"
-     />
-     <div className="flex flex-col gap-2">
-     <p className="text-lg font-bold">{activity.item}</p>
-     <div className="flex">
-     <FontAwesomeIcon
-       name="menu"
-       icon={faClock}
-       className="text-lg text-c-white"
-       size="1x"
-     />
-     <p className="pl-2">{activity.time}</p>
-     </div>
-     </div>
-</div>
+      {task.map((activity, index) => (
+      <div className=" bg-c-lightBlue p-5 rounded flex gap-4 items-center">
+        <FontAwesomeIcon
+          name="menu"
+          icon={faCircleDot}
+          className="text-c-white"
+          size="1x"
+        />
+        <div className="flex flex-col gap-2">
+          <p className="text-lg font-bold">{activity.todo}</p>
+          <div className="flex">
+            <FontAwesomeIcon
+              name="menu"
+              icon={faClock}
+              className="text-lg text-c-white"
+              size="1x"
+            />
+            <p className="pl-2">{activity.time}</p>
+          </div>
+        </div>
+      </div>
+))}
 
 
-          
+      {/* form  */}
 
+     {isFormVisible && <form className="flex flex-col" onSubmit={taskHandler}>
+        <label>
+          <input
+          name="todo"
+          required
 
+            type="text"
+            className="bg-c-lightBlue"
+            placeholder="Add a to-do item"
+          />
+        </label>
 
+        <label className="flex flex-rows">
+          <input name="time"
+            required
+ type="time" placeholder="set time" className="" />
 
-        ))}
-       
-       <form  className="flex flex-col">
-          <label>
-            
-            <input type="text" className="bg-c-lightBlue" placeholder="Add a to-do item" />
-          </label>
-        
-          <label className="flex flex-rows">
-            <input type="time" placeholder="set time" className=""/>
-           
-            
-            <button type="submit">Submit</button>
-          </label>
-          
-          
-        </form>
+          <button type="submit"  >Submit</button>
+        </label>
+      </form>} 
 
+      {/* +icon */}
       <FontAwesomeIcon
         name="menu"
         icon={faPlusCircle}
         className="text-c-white"
         size="2x"
-        onClick={taskHandler}
+
+        onClick={() => {setIsFormVisible(true)
+        }}
       />
     </div>
   );
