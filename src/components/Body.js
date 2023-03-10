a799afc2c213502de930196aebd14594129be826
Papi <gj.olaseni@gmail.com>
2023-03-10 16:10:55 +0100

    working on the layout of body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { v4 as uuidv4 } from "uuid";
 
 // fontawesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,84 +11,106 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function Body() {
-//store data with use state
-const [task, setTask] = useState([]
-);
+  //store data with use state
+  const [task, setTask] = useState([]);
 
-//control form visibility
-const [isFormVisible, setIsFormVisible] = useState(false);
+  //control form visibility
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
+  //function to update setTask
+  function taskHandler(event) {
+    event.preventDefault();
+    const newInput = {
+      id: uuidv4(),
+      todo: event.target.todo.value,
+      time: event.target.time.value,
+    };
+    setTask([...task, newInput]);
+    setIsFormVisible(false);
+    event.target.reset();
+    console.log(task);
+  }
 
-
-//function to update setTask
-function taskHandler(event) {
-  event.preventDefault();
-  const newInput = {
-    todo: event.target.todo.value,
-    time: event.target.time.value,
+  //function to delete task
+  const handleDelete = (id) => {
+    setTask(task.filter((obj) => obj.id !== id));
   };
-  setTask([...task,newInput]
-    
-    
-  );
-  setIsFormVisible(false)
-  event.target.reset();
-  console.log(task)
-}
+
+  //local storage
+  useEffect(() => {
+    try {
+      const data = localStorage.getItem("my-task");
+      if (data) {
+        setTask(JSON.parse(data));
+      }
+    } catch (error) {
+      console.log("Error parsing data from localStorage", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    if(task.length !== 0){  localStorage.setItem("my-task", JSON.stringify(task))}
+  }, [task])
 
 
+  
   return (
-    <div className="min-h-screen font-Roboto flex flex-col gap-y-4  items-center">
+    <div className="min-h-screen h-1/2 font-Roboto flex flex-col gap-y-4  items-center">
       <h3 className="text-xl">To-dos</h3>
       {/* How a todo should look like */}
 
-      
       {task.map((activity, index) => (
-      <div className=" bg-c-lightBlue p-5 rounded flex gap-4 items-center">
-        <FontAwesomeIcon
-          name="menu"
-          icon={faCircleDot}
-          className="text-c-white"
-          size="1x"
-        />
-        <div className="flex flex-col gap-2">
-          <p className="text-lg font-bold">{activity.todo}</p>
-          <div className="flex">
-            <FontAwesomeIcon
-              name="menu"
-              icon={faClock}
-              className="text-lg text-c-white"
-              size="1x"
-            />
-            <p className="pl-2">{activity.time}</p>
+        <div className=" bg-c-lightBlue p-5 rounded flex gap-4 items-center  md:w-2/4  min-[280px]:w-3/4">
+          <FontAwesomeIcon
+            name="menu"
+            icon={faCircleDot}
+            className="text-green-600"
+            size="1x"
+            onClick={() => handleDelete(activity.id)}
+          />
+          <div key={activity.id} className="flex flex-col gap-2">
+            <p className="text-lg font-bold">{activity.todo}</p>
+            <div className="flex">
+              <FontAwesomeIcon
+                name="menu"
+                icon={faClock}
+                className="text-lg text-c-white"
+                size="1x"
+              />
+              <p className="pl-2">{activity.time}</p>
+            </div>
           </div>
         </div>
-      </div>
-))}
-
+      ))}
 
       {/* form  */}
 
-     {isFormVisible && <form className="flex flex-col" onSubmit={taskHandler}>
-        <label>
-          <input
-          name="todo"
-          required
+      {isFormVisible && (
+        <form className="  md:w-2/4  min-[280px]:w-3/4 flex flex-col gap-2  " onSubmit={taskHandler}>
+          <label  className="w-full">
+            <input
+              name="todo"
+              required
+              type="text"
+              className="w-full rounded bg-c-lightBlue "
+              placeholder="Add a to-do item"
+            />
+          </label>
 
-            type="text"
-            className="bg-c-lightBlue"
-            placeholder="Add a to-do item"
-          />
-        </label>
+          <label className="flex flex-rows gap-2">
+            <input
+              name="time"
+              required
+              type="time"
+              placeholder="set time"
+              className="w-1/2 rounded text-c-white bg-c-lightBlue"
+            />
+           
 
-        <label className="flex flex-rows">
-          <input name="time"
-            required
- type="time" placeholder="set time" className="" />
-
-          <button type="submit"  >Submit</button>
-        </label>
-      </form>} 
+            <button className="w-1/2 rounded" type="submit">Save</button>
+          </label>
+        </form>
+      )}
 
       {/* +icon */}
       <FontAwesomeIcon
@@ -94,8 +118,8 @@ function taskHandler(event) {
         icon={faPlusCircle}
         className="text-c-white"
         size="2x"
-
-        onClick={() => {setIsFormVisible(true)
+        onClick={() => {
+          setIsFormVisible(true);
         }}
       />
     </div>
